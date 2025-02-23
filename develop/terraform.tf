090752adefd39a6faad38f26a358cf15116032de
YusukeTakatsu <github ys.taka44@gmail.com>
2025-02-23 12:29:35 +0900

    feat:terraform settings for develop

---
+++ develop/terraform.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "1.10.5"
+  backend "s3" {
+    bucket = "event-management-terraform"
+    key    = "develop.terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.44.0"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      Env    = "dev"
+      System = "share-money"
+    }
+  }
+}
