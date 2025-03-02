16e142c8d2804fb1dffb8fb03d048c2cd267c719
YusukeTakatsu <github ys.taka44@gmail.com>
2025-03-02 19:02:29 +0900

    feat:add public subnet

--- develop/vpc.tf
+++ develop/vpc.tf
@@ -12,3 +12,18 @@ resource "aws_vpc" "main" {
     Name = "${local.project}-${local.env}"
   }
 }
+
+# ===============================================================================
+# public subnet
+# ===============================================================================
+resource "aws_subnet" "public" {
+  for_each                = toset(local.availability_zones)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 4, index(local.availability_zones, each.key))
+  availability_zone       = each.key
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${local.project}-${local.env}-public-${each.key}"
+  }
+}
+
