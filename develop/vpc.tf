c0a5e874af397393979a21ce7fbad36337a0204c
YusukeTakatsu <github ys.taka44@gmail.com>
2025-03-08 10:43:32 +0900

    feat:add route table

--- develop/vpc.tf
+++ develop/vpc.tf
@@ -35,3 +35,30 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_route_table" "public" {
+  for_each = toset(local.availability_zones)
+  vpc_id   = aws_vpc.main.id
+
+  lifecycle {
+    ignore_changes = [
+      route,
+    ]
+  }
+
+  tags = {
+    Name = "${local.project}-${local.env}-public-${each.key}"
+  }
+}
+
+resource "aws_route" "default_gw" {
+  for_each = toset(local.availability_zones)
+  route_table_id = aws_route_table.public[each.key].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = toset(local.availability_zones)
+  subnet_id = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public[each.key].id
+}
