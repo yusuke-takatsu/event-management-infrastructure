2eb0c9186d42563396881f5316abe95b610be364
YusukeTakatsu <github ys.taka44@gmail.com>
2025-03-08 09:57:07 +0900

    feat:add internet gatway

--- develop/vpc.tf
+++ develop/vpc.tf
@@ -13,6 +13,14 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.project}-${local.env}"
+  }
+}
+
 # ===============================================================================
 # public subnet
 # ===============================================================================
