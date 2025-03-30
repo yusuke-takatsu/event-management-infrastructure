15c99633f371e2403b91a0d74731c702028bca1f
YusukeTakatsu <github ys.taka44@gmail.com>
2025-03-30 15:10:02 +0900

    feat:add key pair

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,12 @@ data "aws_ssm_parameter" "al2023" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-minimal-kernel-default-arm64"
 }
 
+resource "aws_key_pair" "name" {
+  key_name   = "${local.project}-${local.env}-bastion"
+  public_key = var.key_pair_pub
+}
+
 resource "aws_instance" "basition_al2023" {
-  ami = data.aws_ssm_parameter.al2023.value
+  ami           = data.aws_ssm_parameter.al2023.value
   instance_type = "t2.micro"
 }
